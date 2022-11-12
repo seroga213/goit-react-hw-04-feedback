fc0eea75ecc6202b11453ac35d8290fc666f806c
seroga213 <serogaseroga05@gmail.com>
2022-11-12 18:33:59 +0300

    tired

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,156 @@
 import React from "react";
-import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { FeedbackOptions } from "../components/FeedbackOptions/FeedbackOptions";
+import { Title, Section} from './App.styled';
 import { Statistics } from '../components/Statictics/Statistics';
-// import { StatisticsTitle } from '../components/Statictics/Statistics.styled';
+// import { StatisticsTitle, StatisticsCard } from '../components/Statictics/Statistics.styled';
 import { Notification } from 'components/Notification/Notification';
-import  Section  from './Section/Section';
+import { useState } from "react";
 
 
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
 
-export class Feedback extends React.Component{
-    state = { 
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }  
-    handleIncrement = (e) => {
+  const state = { good, neutral, bad }
+  
 
-        let curentfeedback = e.currentTarget.name;
+  const handleIncrement = type => {
 
-        this.setState((prevState) => ({[curentfeedback] : prevState[curentfeedback] + 1}))
-    }
+    const target = type.target.name;
+    switch(target) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      default:
+        break;
+  }
+  
 
-    countTotalFeedback=() => {
-        let countTotal = this.state.good + this.state.neutral + this.state.bad;
-
-        return countTotal;
-
-    }
-
-    countPositiveFeedbackPercentage = () =>{
-        let percentageGood = Math.round(this.state.good / this.countTotalFeedback() * 100);
-        
-        return percentageGood;
-
-    }
-
-    render() {
-        let totalFeedback = this.countTotalFeedback();
-        return (
-          <div
-          style={{
-            height: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            fontSize: 20,
-            color: '#010101'
-
-              }}
-            >
-        <Section title = "Please leave feedback">
-                <FeedbackOptions
-                    options = {Object.keys(this.state)}
-                    addFeedback ={this.handleIncrement}
-                ></FeedbackOptions>
-    
-        </Section>
-
-        <Section title = "Statistics" >
-            
-                    {totalFeedback === 0
-                        ? <Notification message="There is no feedback"></Notification>
-                        : <Statistics
-                            good={this.state.good}
-                            neutral={this.state.neutral}
-                            bad={this.state.bad}
-                            totalFeedback={this.countTotalFeedback()}
-                            percentage={this.countPositiveFeedbackPercentage()}  
-                            >
-                        </Statistics>}
-                
-                </Section>
-        
-        </div>
-    )}
 }
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  
+  
+  const countPositiveFeedbackPercentage = () => {
+    let total = good + neutral + bad;
+    if (total === 0) {
+      return 0;
+    } else {
+      return Math.trunc((good / total) * 100);
+    }
+  }
+  return (
+    <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      fontSize: 20,
+      color: '#010101'
+
+        }}>
+      <Section title = "Please leave feedback">
+        {/* <Title>Please leave feedback</Title> */}
+              <FeedbackOptions 
+                  options={Object.keys(state)}
+                  addFeedback ={handleIncrement}
+              ></FeedbackOptions>
+          </Section>
+          <Section>
+                <Title>Statistics</Title>
+                  {countTotalFeedback() === 0
+                      ? <Notification message="There is no feedback"></Notification>
+                      : <Statistics
+                          ValueGood={good}
+                          ValueNeutral={neutral}
+                          ValueBad={bad}
+                          totalFeedback={countTotalFeedback()}
+                          percentage={countPositiveFeedbackPercentage()}>
+                      </Statistics>}
+              
+          </Section>
+          
+      </div>
+  )
+}
+
+
+
+// export class Feedback extends React.Component{
+//     state = { 
+//         good: 0,
+//         neutral: 0,
+//         bad: 0
+//     }  
+//     handleIncrement = (e) => {
+
+//         let curentfeedback = e.currentTarget.name;
+
+//         this.setState((prevState) => ({[curentfeedback] : prevState[curentfeedback] + 1}))
+//     }
+
+//     countTotalFeedback=() => {
+//         let countTotal = this.state.good + this.state.neutral + this.state.bad;
+
+//         return countTotal;
+
+//     }
+
+//     countPositiveFeedbackPercentage = () =>{
+//         let percentageGood = Math.round(this.state.good / this.countTotalFeedback() * 100);
+        
+//         return percentageGood;
+
+//     }
+
+//     render() {
+//         let totalFeedback = this.countTotalFeedback();
+//         return (
+//           <div
+//           style={{
+//             height: '100vh',
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//             fontSize: 20,
+//             color: '#010101'
+
+//               }}
+//             >
+//         <Section title = "Please leave feedback">
+//                 <FeedbackOptions
+//                     options = {Object.keys(this.state)}
+//                     addFeedback ={this.handleIncrement}
+//                 ></FeedbackOptions>
+    
+//         </Section>
+
+//         <Section title = "Statistics" >
+            
+//                     {totalFeedback === 0
+//                         ? <Notification message="There is no feedback"></Notification>
+//                         : <Statistics
+//                             good={this.state.good}
+//                             neutral={this.state.neutral}
+//                             bad={this.state.bad}
+//                             totalFeedback={this.countTotalFeedback()}
+//                             percentage={this.countPositiveFeedbackPercentage()}  
+//                             >
+//                         </Statistics>}
+                
+//                 </Section>
+        
+//         </div>
+//     )}
+// }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,23 +1,36 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { Container, ContainerListBtm, ContainerList } from './FeedbackOptions.styled';
+import { Container, ContainerListBtm } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = ({ options, addFeedback }) => {
-    
     return (
         <Container>
-            {options.map(item => (
-                <ContainerList key={item} >
-                    <ContainerListBtm type="button" name={item} onClick={addFeedback}>{item}
-                    </ContainerListBtm>
-                </ContainerList>
-            ))}
-        </Container>
-    )
+        {options.map(name => {
+            return (
+                <ContainerListBtm
+            key = { name }
+            name = { name }
+            onClick = {addFeedback}
+        >
+        {name}
+    </ContainerListBtm>
+);
+        })}
+    </Container>
+    );
 }
+//         <Container>
+//             {options.map(item => (
+//                 <ContainerList key={item} >
+//                     <ContainerListBtm type="button" name={item} onClick={addFeedback}>{item}
+//                     </ContainerListBtm>
+//                 </ContainerList>
+//             ))}
+//         </Container>
+//     )
+// }
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.string.isRequired,
-    // options: PropTypes.arrayOf(PropTypes.string.isRequired),
+    options: PropTypes.objectOf(PropTypes.number.isRequired).isRequired,
     addFeedback: PropTypes.func.isRequired,
 }
